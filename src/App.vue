0f4c55a5f4ebf37b7572f419fd6b7f56aeaa94da
Theam-Dev <phearakroun@gmail.com>
2023-06-01 20:33:53 +0700

    Image Copare

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>img comparison slider</h2>
+    <img-comparison-slider>
+      <img slot="first" src="./assets/3.jpg" width="510"/>
+      <img slot="second" src="./assets/4.jpg" width="510"/>
+    </img-comparison-slider>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'img-comparison-slider';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'img-comparison-slider';
 
 Vue.config.productionTip = false
-
+Vue.config.ignoredElements = [/img-comparison-slider/];
 new Vue({
   render: h => h(App),
 }).$mount('#app')
